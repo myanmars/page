0049d6a6810f72df6fd7af7bf897d1a774b832f9
panna <145360237+myanmars@users.noreply.github.com>
2023-10-12 17:02:58 +0630

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-const camera = document.getElementById('camera');
+const webrtc = new SimpleWebRTC({
+    localVideoEl: 'localVideo',
+    remoteVideosEl: 'remoteVideos',
+    autoRequestMedia: true
+});
 
-// Check for browser support
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    // Access the webcam camera
-    navigator.mediaDevices
-        .getUserMedia({ video: true })
-        .then(function (stream) {
-            camera.srcObject = stream;
-        })
-        .catch(function (error) {
-            console.error('Error accessing the camera:', error);
-        });
-} else {
-    console.error('getUserMedia is not supported by this browser');
-}
+webrtc.on('readyToCall', function () {
+    webrtc.joinRoom('your-room-name');
+});
