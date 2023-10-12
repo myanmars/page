96009308a2a861931d06955721288ab2e674efb3
panna <145360237+myanmars@users.noreply.github.com>
2023-10-12 17:13:45 +0630

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-const webrtc = new SimpleWebRTC({
-    localVideoEl: 'localVideo',
-    remoteVideosEl: 'remoteVideos',
-    autoRequestMedia: true
-});
+// JavaScript (app.js)
 
-webrtc.on('readyToCall', function () {
-    webrtc.joinRoom('your-room-name');
-});
+// Function to send a chat message
+function sendMessage() {
+    const chatInput = document.getElementById('chatInput');
+    const message = chatInput.value;
+    chatInput.value = ''; // Clear the input field
+
+    // Display the message in the chat box
+    const chatMessages = document.getElementById('chatMessages');
+    const messageElement = document.createElement('div');
+    messageElement.textContent = message;
+    chatMessages.appendChild(messageElement);
+}
+
+// Set up event listener for the "Send" button
+const sendButton = document.getElementById('sendButton');
+sendButton.addEventListener('click', sendMessage);
+
+// Function to start video streaming
+function startVideo() {
+    // Access the local camera and microphone using WebRTC
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+        .then(function (stream) {
+            const localVideo = document.getElementById('localVideo');
+            localVideo.srcObject = stream;
+        })
+        .catch(function (error) {
+            console.error('Error accessing camera and microphone:', error);
+        });
+}
+
+// Set up event listener for the "Start Video" button (if you have one)
+const startVideoButton = document.getElementById('startVideoButton');
+startVideoButton.addEventListener('click', startVideo);
