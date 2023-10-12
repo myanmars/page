39b18c405e77222c2ee0261cea76daed9c79196c
panna <145360237+myanmars@users.noreply.github.com>
2023-10-12 16:14:51 +0630

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,60 @@
-const camera1 = document.getElementById('camera1');
-const camera2 = document.getElementById('camera2');
-const camera3 = document.getElementById('camera3');
+// JavaScript (app.js)
+
+const localVideo = document.getElementById('localVideo');
+const remoteVideo = document.getElementById('remoteVideo');
 const startButton = document.getElementById('startButton');
-const stopButton = document.getElementById('stopButton');
+const hangupButton = document.getElementById('hangupButton');
 
-let stream1;
-let stream2;
-let stream3;
+let localStream;
+let remoteStream;
+let peerConnection;
 
-startButton.addEventListener('click', startCameras);
-stopButton.addEventListener('click', stopCameras);
+startButton.addEventListener('click', startVideoChat);
+hangupButton.addEventListener('click', hangUp);
 
-async function startCameras() {
+// Function to start the video chat
+async function startVideoChat() {
     try {
-        stream1 = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-        stream2 = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-        stream3 = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        // Access the local camera and microphone
+        localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        localVideo.srcObject = localStream;
 
-        camera1.srcObject = stream1;
-        camera2.srcObject = stream2;
-        camera3.srcObject = stream3;
+        // Create a peer connection
+        peerConnection = new RTCPeerConnection();
+
+        // Add the local stream to the peer connection
+        localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
+
+        // Set up event handlers for the peer connection
+        peerConnection.ontrack = handleRemoteStream;
+
+        // Create an offer and set it as the local description
+        const offer = await peerConnection.createOffer();
+        await peerConnection.setLocalDescription(offer);
+
+        // Simulate signaling by sending the offer to the other peer
+        const offerData = JSON.stringify(offer);
+        // Send offerData to the other peer (you'll need signaling server for this in a real application)
+
     } catch (error) {
-        console.error('Error starting cameras:', error);
+        console.error('Error starting the video chat:', error);
     }
 }
 
-function stopCameras() {
-    if (stream1) {
-        stream1.getTracks().forEach(track => track.stop());
-        camera1.srcObject = null;
-    }
-    if (stream2) {
-        stream2.getTracks().forEach(track => track.stop());
-        camera2.srcObject = null;
-    }
-    if (stream3) {
-        stream3.getTracks().forEach(track => track.stop());
-        camera3.srcObject = null;
-    }
+// Function to handle incoming remote stream
+function handleRemoteStream(event) {
+    remoteStream = event.streams[0];
+    remoteVideo.srcObject = remoteStream;
 }
+
+// Function to hang up the video chat
+function hangUp() {
+    localStream.getTracks().forEach(track => track.stop());
+    if (peerConnection) {
+        peerConnection.close();
+    }
+    localVideo.srcObject = null;
+    remoteVideo.srcObject = null;
+}
+
+// You will need signaling server to exchange SDPs and ICE candidates with the other peer
